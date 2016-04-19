670d5639a81e3a3c0300c8f1fddac535a8a7180d
itteforsberg <itteforsberg@gmail.com>
2016-04-19 15:01:48 +0200

    Videre.

--- HelloPython/Main.py
+++ HelloPython/Main.py
@@ -1,13 +1,29 @@
 fett = 23
 print(fett)
 
-name = input("What's your name? ")
-print("Nice to meet you " + name + "!")
+#name = input("What's your name? ")
+#print("Nice to meet you " + name + "!")
 
-person = input("Nationality? ")
-if person == "french":
-    print("Préférez-vous parler français?")
-elif person == "norsk":
-    print("Detta er fett!")
-else:
-    print("Good afternoon")
+#person = input("Nationality? ")
+#if person == "french":
+#    print("Préférez-vous parler français?")
+#elif person == "norsk":
+#    print("Detta er fett!")
+#else:
+#    print("Good afternoon")
+
+a = 5
+b = 13
+
+maks = a if (a > b) else b
+
+print(maks)
+
+
+x = float(input("1st Number: "))
+y = float(input("2nd Number: "))
+z = float(input("3rd Number: "))
+
+maximum = max((x, y, z))
+
+print("The maximal value is: " + str(maximum))
