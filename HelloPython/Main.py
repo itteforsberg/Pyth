a60f7d8bb047cd5943b649ba9fc672c3f72020c8
itteforsberg <itteforsberg@gmail.com>
2016-04-19 15:37:59 +0200

    Videre.

--- HelloPython/Main.py
+++ HelloPython/Main.py
@@ -1,16 +1,7 @@
+#!/usr/bin/env python3
+
 fett = 23
 print(fett)
-
-#name = input("What's your name? ")
-#print("Nice to meet you " + name + "!")
-
-#person = input("Nationality? ")
-#if person == "french":
-#    print("Préférez-vous parler français?")
-#elif person == "norsk":
-#    print("Detta er fett!")
-#else:
-#    print("Good afternoon")
 
 a = 5
 b = 13
@@ -19,11 +10,21 @@ maks = a if (a > b) else b
 
 print(maks)
 
+#x = float(input("1st Number: "))
+#y = float(input("2nd Number: "))
+#z = float(input("3rd Number: "))
 
-x = float(input("1st Number: "))
-y = float(input("2nd Number: "))
-z = float(input("3rd Number: "))
+#maximum = max((x, y, z))
 
-maximum = max((x, y, z))
+#print("The maximal value is: " + str(maximum))
 
-print("The maximal value is: " + str(maximum))
+n = 100
+
+s = 0
+counter = 1
+while counter <= n:
+    s = s + counter
+    print(s)
+    counter += 1
+
+print("Sum of 1 until %d: %d" % (n,s))
